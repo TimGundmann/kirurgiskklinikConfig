9686af6498fe613b63858b8352b69627c7f246b1
tim <tim@gundmann.dk>
2015-04-09 21:23:17 +0200

    boot file updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y tomcat7
+apt-get install -y tomcat7 tomcat7-admin
 
 sudo rm -rf /var/lib/tomcat7/webapps/ROOT
 sudo ln -s /vagrant/kenneth /var/lib/tomcat7/webapps/ROOT
+echo "<?xml version='1.0' encoding='utf-8'?><tomcat-users><role rolename='manager-gui'/><user username='tim' password='Prut1234' roles='manager-gui'/></tomcat-users>" | sudo tee /var/lib/tomcat7/conf/tomcat-users.xml
