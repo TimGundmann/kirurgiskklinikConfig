5cb19f501a01fcfbcc0b69543a6520405a322e8d
tim <tim@gundmann.dk>
2015-04-11 18:34:35 +0200

    java 7 added

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 apt-get update
+apt-get remove openjdk-6-jre-headless
+apt-get remove openjdk-6-jre-lib
+apt-get install -y openjdk-7-jre 
 apt-get install -y tomcat7 tomcat7-admin
 
-#sudo rm -rf /var/lib/tomcat7/webapps/ROOT
-#sudo ln -s /vagrant/kenneth /var/lib/tomcat7/webapps/ROOT
 echo "<?xml version='1.0' encoding='utf-8'?><tomcat-users><role rolename='manager-gui'/><role rolename='manager-script'/><user username='tim' password='Prut1234' roles='manager-gui, manager-script'/></tomcat-users>" | sudo tee /var/lib/tomcat7/conf/tomcat-users.xml
 
 sudo chown root:tomcat7 /var/lib/tomcat7/conf/tomcat-users.xml
