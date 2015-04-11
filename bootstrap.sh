2e4c9417b5902e32300293d294ef6d427d058e82
tim <tim@gundmann.dk>
2015-04-11 11:31:31 +0200

    restart of tomcat

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,10 @@
 apt-get update
 apt-get install -y tomcat7 tomcat7-admin
 
-sudo rm -rf /var/lib/tomcat7/webapps/ROOT
-sudo ln -s /vagrant/kenneth /var/lib/tomcat7/webapps/ROOT
+#sudo rm -rf /var/lib/tomcat7/webapps/ROOT
+#sudo ln -s /vagrant/kenneth /var/lib/tomcat7/webapps/ROOT
 echo "<?xml version='1.0' encoding='utf-8'?><tomcat-users><role rolename='manager-gui'/><user username='tim' password='Prut1234' roles='manager-gui'/></tomcat-users>" | sudo tee /var/lib/tomcat7/conf/tomcat-users.xml
+
+sudo chown root:tomcat7 /var/lib/tomcat7/conf/tomcat-users.xml
+
+sudo service tomcat7 restart
