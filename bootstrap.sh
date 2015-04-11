0115943c812186a2dbd54191f2be67da8510bc06
tim <tim@gundmann.dk>
2015-04-11 11:43:24 +0200

    rool addet to tomcat

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ apt-get install -y tomcat7 tomcat7-admin
 
 #sudo rm -rf /var/lib/tomcat7/webapps/ROOT
 #sudo ln -s /vagrant/kenneth /var/lib/tomcat7/webapps/ROOT
-echo "<?xml version='1.0' encoding='utf-8'?><tomcat-users><role rolename='manager-gui'/><user username='tim' password='Prut1234' roles='manager-gui'/></tomcat-users>" | sudo tee /var/lib/tomcat7/conf/tomcat-users.xml
+echo "<?xml version='1.0' encoding='utf-8'?><tomcat-users><role rolename='manager-gui'/><role rolename="manager-script"/><user username='tim' password='Prut1234' roles='manager-gui, manager-script'/></tomcat-users>" | sudo tee /var/lib/tomcat7/conf/tomcat-users.xml
 
 sudo chown root:tomcat7 /var/lib/tomcat7/conf/tomcat-users.xml
 
