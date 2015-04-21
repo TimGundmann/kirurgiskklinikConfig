d909c50f2c483e47d44cea786b4696a85b3dee1d
tim <tim@gundmann.dk>
2015-04-21 22:18:19 +0200

    context path change for tomcat root

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ apt-get install -y tomcat7 tomcat7-admin
 
 echo "<?xml version='1.0' encoding='utf-8'?><tomcat-users><role rolename='manager-gui'/><role rolename='manager-script'/><user username='tim' password='Prut1234' roles='manager-gui, manager-script'/></tomcat-users>" | sudo tee /var/lib/tomcat7/conf/tomcat-users.xml
 
+sudo cp -f /vagrant/server.xml /var/lib/tomcat7/conf/
+
 sudo chown root:tomcat7 /var/lib/tomcat7/conf/tomcat-users.xml
 
 sudo service tomcat7 restart
